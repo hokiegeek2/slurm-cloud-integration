c527e0f64064003576dd307d79d7a0a9c28e88e7
hokiegeek2 <hokiegeek2@gmail.com>
2021-07-21 12:35:46 -0400

    Removed hard-coded configs, updated helm chart files accordingly

--- src/scripts/start-slurm-jupyter.sh
+++ src/scripts/start-slurm-jupyter.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# set munge.key ownership and permissions
-sudo mv /tmp/munge/munge.key /etc/munge/
+# copy munge.key, set ownership and permissions, and move to config dir
+sudo cp /tmp/munge/munge.key /tmp/munge.key
+sudo mv /tmp/munge.key /etc/munge/munge.key
 sudo chown munge:munge /etc/munge/munge.key
 sudo chmod 400 /etc/munge/munge.key
 
 # start munge authorization service
 sudo service munge start
 
-jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
+jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' \
+            --NotebookApp.password=''
 
 tail -f /dev/null
