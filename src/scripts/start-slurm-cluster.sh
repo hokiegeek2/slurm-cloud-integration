35ae67e110ae66469393e6e86c58cdd43bb7542e
hokiegeek2 <hokiegeek2@gmail.com>
2021-07-21 09:48:35 -0400

    renamed script, added fix for slurm startup race condition, updated test.slurm so it runs long enough to observe in slurm queue manager

---
+++ src/scripts/start-slurm-cluster.sh
@@ -0,0 +1,20 @@
+# Replace munge.key with munge.key file used in calling slurm client(s)
+sudo cp /tmp/munge.key /tmp/m.key
+sudo chown munge:munge /tmp/m.key
+sudo mv /tmp/m.key /etc/munge/munge.key
+sudo chmod 400 /etc/munge/munge.key
+
+# Restart munge service to load replacement munge.key
+sudo service munge start
+
+# Start and configure mysql database and slurm db interface
+sudo service mysql start
+sudo service slurmdbd start
+sudo mysql -u root < initialize-mariadb.sh
+
+# Start slurmd worker
+slurmd
+
+# Sleep for 10 seconds to prevent race condition, then start slurmctld controller
+sleep 10
+slurmctld -D
--- src/scripts/start-slurmctld.sh
+++
@@ -1,7 +0,0 @@
-sudo service munge start
-sudo service mysql start
-sudo service slurmdbd start
-sudo mysql -u root < initialize-mariadb.sh
-
-slurmd
-slurmctld -D
