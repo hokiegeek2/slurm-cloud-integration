74d9cbba6d2629f8114beb5838c7366d5beb85f4
hokiegeek2 <hokiegeek2@gmail.com>
2021-10-21 16:51:38 +0000

    Added support for deploying slurm-jupyter via jupyterhub on k8s

---
+++ src/scripts/initialize-notebook-environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# copy munge.key, set ownership and permissions, and move to config dir
+sudo cp /tmp/munge/munge.key /tmp/munge.key
+sudo mv /tmp/munge.key /etc/munge/munge.key
+sudo chown munge:munge /etc/munge/munge.key
+sudo chmod 400 /etc/munge/munge.key
+
+# start munge authorization service
+sudo service munge start
